dc07d1d149c5ace3524f8e9c28ef97de1bfd490d
satchelspencer <satchelspencer@gmail.com>
2015-12-07 16:13:45 -0700

    new UI for chat

--- server/data/websockets.js
+++ server/data/websockets.js
@@ -1,33 +1,8 @@
-var websocket = function(host, protocol){
+var websocket = function(host){
 	if(!window.WebSocket) return false; //websockets not supported
 	var socket = new WebSocket(host);
 	/* object with command listeners */
 	var events = {};
-	var error = function(){};
-	var ready = function(){};
-	/* public api */
-	var api = {
-		send : function(command, body){
-			socket.send(protocol+':'+command+':'+encodeURIComponent(body));
-			return api;
-		},
-		on : function(command, callback){
-			events[command] = callback;
-			return api;
-		},
-		off : function(command){
-			delete events[command];
-			return api;
-		},
-		error : function(callback){
-			error = callback;
-			return api;
-		},
-		ready : function(callback){
-			ready = callback;
-			return api;
-		},
-	}
 
 	socket.onopen = function(evt) {
 		/* establish credentials with server */
@@ -36,12 +11,32 @@ var websocket = function(host, protocol){
 	socket.onmessage = function(evt){
 		var chunks = evt.data.split(':');
 		/* listen for response from initial connect */
-		if(chunks[0] == 'global' && chunks[1] == 'connect'){
-			/* if message is not 'success' callback with error */
-			if(chunks[2] != 'success') error(chunks[2]);
-			else ready();
-		}else if(chunks[0] == protocol && events[chunks[1]]) events[chunks[1]](chunks[2]);
+		if(events[chunks[0]] && events[chunks[0]][chunks[1]]) events[chunks[0]][chunks[1]](chunks[2]);
 		/* ^^ otherwise check if the message is for the protocol and being listened for */
 	};
-	return api;
+	return {
+		in : function(protocol){
+			events[protocol] = events[protocol]||{};
+			/* public api */
+			var api = {
+				send : function(command, body){
+					socket.send(protocol+':'+command+':'+encodeURIComponent(body));
+					return api;
+				},
+				on : function(command, callback){
+					events[protocol][command] = callback;
+					return api;
+				},
+				off : function(command){
+					delete events[protocol][command];
+					return api;
+				}
+			}
+			return api;
+		},
+		leave : function(){
+			socket.send('global:disconnect:'+document.cookie.split('=')[1]);
+			socket.close();
+		}
+	}
 }
